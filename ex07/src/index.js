b6f30cdbb75076fee07dfe3a1e144370cc375a67
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-03-25 23:07:16 +0100

    Exercise 07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,4 +1,8 @@
 let numberArray = new Set([9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5]);
-console.log(numberArray);
-let uniqueArray = new Set([]);
+let uniqueArray = [];
+for(let set of numberArray) {
+uniqueArray.push(set);
+}
+uniqueArray.sort((a,b) => a - b);
+console.log(uniqueArray);
 module.exports = { numberArray, uniqueArray};
