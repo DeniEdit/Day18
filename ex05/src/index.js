db547b99e84793844b617acb875e408ebec7d548
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-03-26 10:25:22 +0100

    Exercise 05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,20 +1,20 @@
-let shopingList = new Map([
+let shoppingList = new Map([
     ['Banana', 3],
     ['Pineapple', 5],
     ['Pear', 2],
     ['Carrot', 10],
     ['Apple', 1.5],
 ]);
-for(let col of shopingList.entries()) {
+for(let col of shoppingList.entries()) {
     console.log(`${col[0]}: ${col[1]}`);
 }
 
 let arrKey = [];
-arrKey = new Map(shopingList);
+arrKey = new Map(shoppingList);
 console.log(Array.from(arrKey));
 console.log([...arrKey]);
 let arrValue = [];
 arrValue = new Map(arrKey);
 console.log(Array.from(arrValue));
 console.log([...arrValue]);
-module.exports = { shopingList, arrKey, arrValue };
+module.exports = { shoppingList, arrKey, arrValue };
