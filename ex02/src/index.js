e0bd538586e699d320f87a2dd0cc73f299e55958
DeniEdit <41287220+DeniEdit@users.noreply.github.com>
2021-03-26 07:37:30 +0100

    Exercise 02

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,25 +1,37 @@
+let fifteenArray = [];
 function* myGenerator() {
     function* insideGenerator1() {
-        for(let i = 1; i <= 5; i++) 
+        var x;
+        for(i = 1; i <= 5; i++)  { 
+            x=i; 
             yield x;
-    }
-    function* insideGenerator2() {
-        yield* myGenerator();
-        for(let i = 10; i <= 15; i++) 
-        yield x;
-    }
-    function* insideGenerator3() {
-        yield* insideGenerator1();
-        for(let i = 6; i <= 9; i++) 
-            yield x;
+        fifteenArray.push(x);
     }
 }
+    function* insideGenerator2() {
+        for( i = 10; i <= 15; i++) {
+         x=i;
+        yield x;
+        fifteenArray.push(x);
+    }
+}
+    function* insideGenerator3() {
+        for( i = 6; i <= 9; i++) {
+            x = i;
+            yield x;
+           fifteenArray.push(x);
+    }
+}
+
+function* myGenerator() {
+     yield insideGenerator1();
+     yield insideGenerator2();
+     yield insideGenerator3();
+}
+
 const iterator = myGenerator();
-console.log(iterator.next().value);
-let fifteenArray = [];
-let x;
-for(let i=0; i < x; i++) {
-    console.log(iterator.next().value);
+for(i=0; i < 15; i++) {
+    console.log(iterator.next().fifteenArray.join('#, ));
 }
 
 module.exports = { fifteenArray, myGenerator };
